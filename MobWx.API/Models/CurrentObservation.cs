e517b8b5d8ca714ae8ed0c3731060ac223974c6a
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-03 12:31:05 -0800

    CurrentObservation param types updates

--- MobWx.API/Models/CurrentObservation.cs
+++ MobWx.API/Models/CurrentObservation.cs
@@ -4,33 +4,35 @@ namespace MobWx.API.Models;
 
 public partial class CurrentObservation
 {
+    public string Location { get; set; } = string.Empty;
+    public double StationElevation { get; set; } = 0.0d;
     public string Station { get; set; } = string.Empty;
     public DateTime Timestamp { get; set; }
     public string RawMessage { get; set; } = string.Empty; // could be METAR
     public string Description { get; set; } = string.Empty;
     public string IconUrl { get; set; } = string.Empty;
-    public int? TemperatureC { get; set; }
+    public double? TemperatureC { get; set; }
     public int? TemperatureF { get; set; }
-    public int? DewpointC { get; set; }
+    public double? DewpointC { get; set; }
     public int? DewpointF { get; set; }
     public int? WindDirection { get; set; }
-    public int? WindSpeedKph { get; set; }
+    public double? WindSpeedKph { get; set; }
     public int? WindSpeedMph { get; set; }
-    public int? WindGustKph { get; set; }
+    public double? WindGustKph { get; set; }
     public int? WindGustMph { get; set; }
     public int? PressureMb { get; set; } // whole millimeters
     public double? PressureIn { get; set; } // proportianal inches to the tenths
     public int? VisibilityMeters { get; set; }
     public int? VisibilityMiles { get; set; }
-    public int? MaxTemperatureC { get; set; } // 24hr period
+    public double? MaxTemperatureC { get; set; } // 24hr period
     public int? MaxTemperatureF { get; set; } // 24hr period
-    public int? MinTemperatureC { get; set; } // 24hr period
+    public double? MinTemperatureC { get; set; } // 24hr period
     public int? MinTemperatureF { get; set; } // 24hr period
     public int? PrecipitationMmHr { get; set; } // 1hr period
     public double? PrecipitationInchHr { get; set; } // 1hr period
-    public int? WindChillC { get; set; }
+    public double? WindChillC { get; set; }
     public int? WindChillF { get; set; }
-    public int? HeatIndexC { get; set; }
+    public double? HeatIndexC { get; set; }
     public int? HeatIndexF { get; set; }
     public List<CloudLayer>? CloudLayers { get; set; }
 
@@ -99,77 +101,28 @@ public partial class CurrentObservation
             RawMessage = observation.RawMessage ?? string.Empty,
             Description = observation.TextDescription ?? string.Empty,
             IconUrl = observation.Icon ?? string.Empty,
-            //TemperatureC = observation.Temperature?.ToInt(),
             TemperatureC = temperatureC,
-            //TemperatureF = observation.Temperature is not null
-            //    ? CurrentObservation.ToFarenheit(observation.Temperature.ToDouble())
-            //    : null,
             TemperatureF = temperatureF,
-            //DewpointC = observation.Dewpoint?.ToInt(),
             DewpointC = dewpointC,
-            //DewpointF = observation.Dewpoint is not null
-            //    ? CurrentObservation.ToFarenheit(observation.Dewpoint.ToDouble())
-            //    : null,
             DewpointF = dewpointF,
-            //WindDirection = observation.WindDirection?.ToInt(),
             WindDirection = windDirection,
-            //WindSpeedKph = observation.WindSpeed?.ToInt(),
             WindSpeedKph = windSpeedKph,
-            //WindSpeedMph = observation.WindSpeed is not null
-            //    ? CurrentObservation.ToMilesPerHour(observation.WindSpeed.ToInt())
-            //    : null,
             WindSpeedMph = windSpeedMph,
-            //WindGustKph = observation.WindGust is not null
-            //    ? observation.WindGust.Value is not null
-            //        ? observation.WindGust.ToInt()
-            //        : observation.WindGust.ToNullableInt()
-            //    : null,
             WindGustKph = windGustKph,
-            //WindGustMph = observation.WindGust is not null
-            //    ? CurrentObservation.ToMilesPerHour(observation.WindGust.ToInt())
-            //    : null,
             WindGustMph = windGustMph,
-            //PressureIn = observation.BarometricPressure?.ToDouble(),
             PressureIn = pressureIn,
-            //PressureMb = observation.BarometricPressure is not null
-            //    ? CurrentObservation.ToMillibars(observation.BarometricPressure.ToDouble())
-            //    : null,
             PressureMb = pressureMb,
-            //VisibilityKm = observation.Visibility?.ToInt(),
             VisibilityMeters = visibilityM,
-            //VisibilityMi = observation.Visibility is not null
-            //    ? CurrentObservation.ToMiles(observation.Visibility.ToInt())
-            //    : null,
             VisibilityMiles = visibilityMi,
-            //MaxTemperatureC = observation.MaxTemperatureLast24Hours?.ToInt(),
             MaxTemperatureC = maxTemperatureC,
-            //MaxTemperatureF = observation.MaxTemperatureLast24Hours is not null
-            //    ? CurrentObservation.ToFarenheit(observation.MaxTemperatureLast24Hours.ToInt())
-            //    : null,
             MaxTemperatureF = maxTemperatureF,
-            //MinTemperatureC = observation.MinTemperatureLast24Hours?.ToInt(),
             MinTemperatureC = minTemperatureC,
-            //MinTemperatureF = observation.MinTemperatureLast24Hours is not null
-            //    ? CurrentObservation.ToFarenheit(observation.MinTemperatureLast24Hours.ToInt())
-            //    : null,
             MinTemperatureF = minTemperatureF,
-            //PrecipitationMmHr = observation.PrecipitationLastHour?.ToInt(),
             PrecipitationMmHr = precipitationMmHr,
-            //PrecipitationInchHr = observation.PrecipitationLastHour is not null
-            //    ? CurrentObservation.ToInches(observation.PrecipitationLastHour.ToInt())
-            //    : null,
             PrecipitationInchHr = precipitationInchesHour,
-            //WindChillC = observation.WindChill?.ToInt(),
             WindChillC = windChillC,
-            //WindChillF = observation.WindChill is not null
-            //    ? CurrentObservation.ToFarenheit(observation.WindChill.ToInt())
-            //    : null,
             WindChillF = windChillF,
-            //HeatIndexC = observation.HeatIndex?.ToInt(),
             HeatIndexC = heatIndexC,
-            //HeatIndexF = observation.HeatIndex is not null
-            //    ? CurrentObservation.ToFarenheit(observation.HeatIndex.ToInt())
-            //    : null,
             HeatIndexF = heatIndexF,
             CloudLayers = observation.CloudLayers
         };
