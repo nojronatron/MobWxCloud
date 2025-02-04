b37a3d2f83541152ce9eb88f663b40a8fcb95591
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-04 07:23:14 -0800

    Fix MaxTemp24 tostring method.

--- MobWx.Lib/Models/MaxTemperatureLast24Hours.cs
+++ MobWx.Lib/Models/MaxTemperatureLast24Hours.cs
@@ -4,11 +4,6 @@ public class MaxTemperatureLast24Hours : Measurement
 {
     public override string ToString()
     {
-        if (string.IsNullOrWhiteSpace(UnitCode))
-        {
-            UnitCode = ":null";
-        }
-
         return Value is null ? string.Empty : Value.Value.ToString("F2");
     }
 }
