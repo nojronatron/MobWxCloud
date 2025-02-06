70f7134b35e650609f15af646da6a8aae373f992
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-06 14:47:31 -0800

    Add QuantitativeValue models.

---
+++ MobWx.Lib/Models/QuantitativeValueInt.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MobWx.Lib.Models
+{
+    public class QuantitativeValueInt : MeasurementInt
+    {
+        [JsonPropertyName("maxValue")]
+        public double? MaxValue { get; set; }
+
+        [JsonPropertyName("minValue")]
+        new public double? MinValue { get; set; }
+
+        public override string ToString()
+        {
+            return Value?.ToString("F1") ?? string.Empty;
+        }
+    }
+}
