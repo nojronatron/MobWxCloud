f78dec581d23d5df406d2a184b98c36de26753ff
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-06 14:46:18 -0800

    Remove unused constructor.

---
+++ MobWx.Lib/Models/RelativeLocation.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MobWx.Lib.Models;
+
+public class RelativeLocation
+{
+    [JsonPropertyName("city")]
+    public string? City { get; set; }
+
+    [JsonPropertyName("state")]
+    public string? State { get; set; }
+
+    [JsonPropertyName("distance")]
+    public QuantitativeValue? Distance { get; set; }
+
+    [JsonPropertyName("bearing")]
+    public QuantitativeValueInt? Bearing { get; set; }
+
+    public string? DistanceKm => Distance is not null && Distance.Value is not null ? $"{Math.Round(Distance.Value.Value / 100, 1)}" : null;
+    public string? DistanceMi => Distance is not null && Distance.Value is not null ? $"{Math.Round(Distance.Value.Value / 160.9344, 1)}" : null;
+    public string? BearingDegrees => Bearing is not null && Bearing.Value is not null ? $"{Bearing.Value.Value}" : null;
+}
--- MobWx.Lib/Models/Temperature.cs
+++ MobWx.Lib/Models/Temperature.cs
@@ -1,7 +1,15 @@
-﻿namespace MobWx.Lib.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace MobWx.Lib.Models;
 
 public class Temperature : Measurement
 {
+    [JsonPropertyName("maxValue")]
+    public double? MaxValue { get; set; }
+
+    [JsonPropertyName("minValue")]
+    new public double? MinValue { get; set; }
+
     public override string ToString()
     {
         return Value?.ToString("F1") ?? string.Empty;
