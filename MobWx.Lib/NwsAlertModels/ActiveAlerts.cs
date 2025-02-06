64520537102d23caeb7dc9639b420f07aa73476f
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-05 20:01:28 -0800

    Add Alert classes for json, http processing.

---
+++ MobWx.Lib/NwsAlertModels/ActiveAlerts.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MobWx.Lib.NwsAlertModels
+{
+
+    public class ActiveAlerts
+    {
+        [JsonPropertyName("title")]
+        public string? AlertTitle { get; set; } // alert sub-headline for UI display
+
+        [JsonPropertyName("updated")]
+        public DateTime? AlertUpdateTimestamp { get; set; } // date-time of last update to the Alert
+
+        [JsonPropertyName("@graph")]
+        public List<ActiveAlert> Graph { get; set; } = []; // list of ActiveAlert objects
+
+        /// <summary>
+        /// Convenience property returns count of active alerts in a graph.
+        /// </summary>
+        public int Count => Graph.Count;
+    }
+}
