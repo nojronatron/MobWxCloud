d5a23758a8f2c808f1aff0c974ec1f2730398beb
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-12 08:42:08 -0800

    Refactor GetPaths methods: points, alerts.

--- MobWx.API/Common/NwsEndpointPaths.cs
+++ MobWx.API/Common/NwsEndpointPaths.cs
@@ -6,18 +6,6 @@ namespace MobWx.API.Common;
 public static class NwsEndpointPaths
 {
     /// <summary>
-    /// Get the relative path for a gridpoint.
-    /// </summary>
-    /// <param name="gridId"></param>
-    /// <param name="gridX"></param>
-    /// <param name="gridY"></param>
-    /// <returns></returns>
-    public static string FoRelativePath(string gridId, string gridX, string gridY)
-    {
-        return $"/gridpoints/{gridId.ToUpper()}/{gridX},{gridY}/stations";
-    }
-
-    /// <summary>
     /// Get the relative path for the latest observation.
     /// </summary>
     /// <param name="stationId"></param>
@@ -31,16 +19,27 @@ public static class NwsEndpointPaths
     /// Get the relative path for a point.
     /// </summary>
     /// <param name="latLon"></param>
-    /// <param name="limit">max: 25, min: 1; default: 12</param>
+    /// <param name="limit"></param>
     /// <returns></returns>
-    public static string PointPath(PositionBase latLon, int? limit)
+    public static string PointPath(PositionBase latLon)
     {
         // if latLon is of type NullPosition return an empty string
         if (latLon is NullPosition)
         {
             return string.Empty;
         }
+        
+        return $"/points/{latLon.Latitude},{latLon.Longitude}";
+    }
 
+    /// <summary>
+    /// Get the relative path for a point. Limit min: 1, max: 25, default: 12.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="limit"></param>
+    /// <returns></returns>
+    public static string GetActiveAlertPath(Position position, int? limit)
+    {
         int limitNum = 12;
 
         if (limit is not null)
@@ -50,29 +49,18 @@ public static class NwsEndpointPaths
                 limitNum = (int)limit;
             }
         }
-        
-        return $"/points/{latLon.Latitude},{latLon.Longitude}?limit={limitNum}";
-    }
-
-    /// <summary>
-    /// Get the relative path for a point. Assumes a limit of 12 alerts.
-    /// </summary>
-    /// <param name="latLon"></param>
-    /// <returns></returns>
-    public static string PointPath(PositionBase latLon)
-    {
-        return PointPath(latLon, null);
+     
+        return $"/alerts/active?point={position.Latitude},{position.Longitude}&limit={limitNum}";
     }
 
     /// <summary>
     /// Accepts a Position object and returns the path for the active alerts endpoint.
-    /// Limited to a maximum of 12 alerts.
+    /// Limits results to maximum of 12.
     /// </summary>
     /// <param name="position"></param>
-    /// <returns>A string path for calling NWS API ActiveAlerts endpoint</returns>
+    /// <returns></returns>
     public static string GetActiveAlertPath(Position position)
     {
-        int limit = 12;
-        return $"/alerts/active?point={position.Latitude},{position.Longitude}&limit={limit}";
+        return GetActiveAlertPath(position, null);
     }
 }
