ea9ea191eb2ed9062c56a45cfc043cfc9b868817
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-13 11:19:39 -0800

    Add geocode data models.

---
+++ MobWx.Lib/Models/Geocoding/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MobWx.Lib.Models.Geocoding;
+
+public class Feature
+{
+    [JsonPropertyName("type")]
+    public string? FeatureType { get; set; }
+
+    [JsonPropertyName("properties")]
+    public FeatureProperties? FeatureProperties { get; set; }
+
+    // [ lon, lat, lon, lat, ..., ... ]
+    [JsonPropertyName("bbox")]
+    public List<double> BBox { get; set; } = [];
+
+    [JsonPropertyName("geometry")]
+    public Geometry? Geometry { get; set; }
+}
---
+++ MobWx.Lib/Models/Geocoding/Geometry.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MobWx.Lib.Models.Geocoding;
+
+public class Geometry
+{
+    [JsonPropertyName("geometry")]
+    public string? GeometryType { get; set; }
+
+    [JsonPropertyName("coordinates")]
+    public List<double> CoordinateDoubles { get; set; } = [];
+
+    /// <summary>
+    /// Get the location of the geometry
+    /// </summary>
+    /// <returns></returns>
+    public Location GetLocation()
+    {
+        return new Location(CoordinateDoubles[1], CoordinateDoubles[0]);
+    }
+
+    /// <summary>
+    /// Get the position of the geometry
+    /// </summary>
+    /// <returns></returns>
+    public Position GetPosition()
+    {
+        return new Position(CoordinateDoubles[1].ToString(), CoordinateDoubles[0].ToString());
+    }
+}
