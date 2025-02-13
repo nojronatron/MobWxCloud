a73a460e4f647790f1761632bb677d9c0dcbdadf
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-13 14:35:16 -0800

    Move custom DI configs to static classes.

---
+++ MobWx.API/ServerConfig/ServicesConfig.cs
@@ -0,0 +1,19 @@
+﻿using MobWx.API.Common;
+using MobWx.API.Endpoints;
+
+namespace MobWx.API.ServerConfig
+{
+    public static class ServicesConfig
+    {
+        public static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<ICurrentConditionsHandler, CurrentConditionsHandler>();
+            builder.Services.AddTransient<INwsEndpointAbstraction, NwsEndpointAbstraction>();
+            builder.Services.AddTransient<IJsonHandler, JsonHandler>();
+            builder.Services.AddTransient<IForecastsHandler, ForecastsHandler>();
+            builder.Services.AddTransient<IAlertsHandler, AlertsHandler>();
+            builder.Services.AddTransient<ILocationHandler, LocationHandler>();
+            builder.Services.AddTransient<IOpenStreetMapsAbstraction, OpenStreetMapsAbstraction>();
+        }
+    }
+}
