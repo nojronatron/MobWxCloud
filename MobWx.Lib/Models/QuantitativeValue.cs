275dab639a30049b82400b8dd0283957ca6a707d
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-02-14 09:00:04 -0800

    Move QV functions to extension methods.

--- MobWx.Lib/Models/QuantitativeValue.cs
+++ MobWx.Lib/Models/QuantitativeValue.cs
@@ -1,63 +1,30 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace MobWx.Lib.Models
+namespace MobWx.Lib.Models;
+
+public class QuantitativeValue
 {
-    public class QuantitativeValue
+    [JsonPropertyName("value")]
+    public double? Value { get; set; }
+
+    [JsonPropertyName("maxValue")]
+    public double? MaxValue { get; set; }
+
+    [JsonPropertyName("minValue")]
+    public double? MinValue { get; set; }
+
+    [JsonPropertyName("unitCode")]
+    public string UnitCode { get; set; } = string.Empty;
+
+    [JsonPropertyName("qualityControl")]
+    public string QualityControl { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Print Value as a string, or an empty string if Value is null.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
     {
-        public virtual double? Value { get; set; }
-
-        [JsonPropertyName("unitCode")]
-        public string UnitCode { get; set; } = string.Empty;
-
-        [JsonPropertyName("qualityControl")]
-        public string QualityControl { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Convert the value to an integer after converting to a value with LESS precision.
-        /// See .NET 9 documentation "Midpoint values and rounding conventions" for more information.
-        /// https://learn.microsoft.com/en-us/dotnet/api/system.math.round?view=net-9.0#midpoint-values-and-rounding-conventions
-        /// </summary>
-        /// <returns></returns>
-        public int ToInt()
-        {
-            return (int)Math.Round((double)Value!, 0);
-        }
-
-        /// <summary>
-        /// Convert a possibly Null value to an integer after converting to a value with LESS precision.
-        /// If Value is Null, return Null.
-        /// </summary>
-        /// <returns></returns>
-        public int? ToNullableInt()
-        {
-            return Value is not null ? (int)Math.Round((double)Value, 0) : null;
-        }
-
-        /// <summary>
-        /// Convert the value to a double.
-        /// </summary>
-        /// <returns></returns>
-        public double ToDouble()
-        {
-            return (double)Math.Round((double)Value!, 2);
-        }
-
-        /// <summary>
-        /// Convert the value to a nullable double.
-        /// </summary>
-        /// <returns></returns>
-        public double? ToNullableDouble()
-        {
-            return Value is not null ? (double)Math.Round((double)Value, 2) : null;
-        }
-
-        /// <summary>
-        /// Convert the value to a string. If value is null, returns "null".
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return Value?.ToString("F1") ?? string.Empty;
-        }
+        return Value.HasValue ? Value.Value.ToString() : string.Empty;
     }
 }
