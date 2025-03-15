2e20c6846c763e7031a1b927f3d45e2f461466c7
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-03-15 09:30:37 -0700

    Update RelativeLocation with geo, decimal.

--- MobWx.Lib/Models/RelativeLocation.cs
+++ MobWx.Lib/Models/RelativeLocation.cs
@@ -2,6 +2,9 @@
 
 namespace MobWx.Lib.Models;
 
+/// <summary>
+/// Defined by the NWS API for use with /points/{point} endpoint response.
+/// </summary>
 public class RelativeLocation
 {
     [JsonPropertyName("city")]
@@ -10,13 +13,16 @@ public class RelativeLocation
     [JsonPropertyName("state")]
     public string? State { get; set; }
 
+    [JsonPropertyName("geometry")]
+    public string? Geometry { get; set; } // POINT(lon lat)
+
     [JsonPropertyName("distance")]
     public QuantitativeValue? Distance { get; set; }
 
     [JsonPropertyName("bearing")]
     public QuantitativeValue? Bearing { get; set; }
 
-    public string? DistanceKm => Distance is not null && Distance.Value is not null ? $"{Math.Round(Distance.Value.Value / 100, 1)}" : null;
-    public string? DistanceMi => Distance is not null && Distance.Value is not null ? $"{Math.Round(Distance.Value.Value / 160.9344, 1)}" : null;
+    public string? DistanceKm => Distance is not null && Distance.Value is not null ? $"{Math.Round(Distance.Value.Value / 100m, 1)}" : null;
+    public string? DistanceMi => Distance is not null && Distance.Value is not null ? $"{Math.Round(Distance.Value.Value / 160.9344m, 1)}" : null;
     public string? BearingDegrees => Bearing is not null && Bearing.Value is not null ? $"{Bearing.Value.Value}" : null;
 }
